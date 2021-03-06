65a8dfcfd44043f0fed7137d998e4694e3478f04
Guilherme <guiihh532@gmail.com>
2021-03-06 15:49:36 -0300

    Service layer, component registration

--- src/main/java/com/gomlek/course/repositories/UserRepository.java
+++ src/main/java/com/gomlek/course/repositories/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gomlek.course.entities.User;
 
+
+
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 }
