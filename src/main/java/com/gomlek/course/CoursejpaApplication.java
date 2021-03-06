3d703c46adf61fbe0535d3f938eb345df3ef7b4e
Guilherme <guiihh532@gmail.com>
2021-03-06 14:38:05 -0300

    Project Created

---
+++ src/main/java/com/gomlek/course/CoursejpaApplication.java
@@ -0,0 +1,13 @@
+package com.gomlek.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursejpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursejpaApplication.class, args);
+	}
+
+}
