5985cead77eb7a8ed37ccb576f03f8bdc5750241
Guilherme <guiihh532@gmail.com>
2021-03-10 17:07:38 -0300

    Exception handling - findById

---
+++ src/main/java/com/gomlek/course/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.gomlek.course.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.gomlek.course.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+}
--- src/main/java/com/gomlek/course/services/UserService.java
+++ src/main/java/com/gomlek/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.gomlek.course.entities.User;
 import com.gomlek.course.repositories.UserRepository;
+import com.gomlek.course.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -24,7 +25,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
